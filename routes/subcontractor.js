3ad5fd1e5b82b0cd7cad8c505b1eeeb3d0bba031
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-06-02 16:44:30 +0100

    Update subcontractor.js

--- routes/subcontractor.js
+++ routes/subcontractor.js
@@ -4,10 +4,19 @@ const express = require('express');
 const router = express.Router();
 const {
     submitOnboardingForm,
-    renderOnboardingForm
-} = require('../controllers/account');
+    renderOnboardingForm,
+} = require('../controllers/onboarding');
+
+const {
+    renderSubcontractorForm,
+    submitSubcontractorForm,
+} = require('../controllers/subcontractor');
 
 router.get('/onboarding', renderOnboardingForm);
 router.post('/onboarding', submitOnboardingForm);
+
+router.get('/subcontractor/create', renderSubcontractorForm);
+router.post('/subcontractor/create', submitSubcontractorForm);
+
 
 module.exports = router;
