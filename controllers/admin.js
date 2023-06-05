ad8fe5d2d7ab68ea350a4358ae6be26ef82d365e
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-06-05 17:49:26 +0100

    Create admin.js

---
+++ controllers/admin.js
@@ -0,0 +1,119 @@
+// controllers/admin.js
+
+const packageJson = require('../package.json');
+const Invoice = require('../models/invoice');
+const Subcontractor = require('../models/subcontractor');
+const User = require('../models/user');
+const helpers = require('../helpers');
+
+// Render the admin dashboard
+const renderAdminDashboard = async (req, res) => {
+    try {
+        const userCount = await User.count();
+        const subcontractorCount = await Subcontractor.count();
+        const invoiceCount = await Invoice.count();
+
+        const users = await User.findAll();
+        const subcontractors = await Subcontractor.findAll();
+        const invoices = await Invoice.findAll();
+
+        //const user = await User.findByPk();
+
+        res.render('adminDashboard', {
+            userCount,
+            subcontractorCount,
+            invoiceCount,
+            users,
+            subcontractors,
+            invoices,
+            errorMessages: req.flash('error'),
+            successMessage: req.flash('success'),
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+            message: req.query.message || '',
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// Delete an invoice
+const deleteInvoice = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can delete invoices.');
+        }
+
+        const invoiceId = req.params.id;
+        const invoice = await Invoice.findByPk(invoiceId);
+
+        if (!invoice) {
+            return res.status(404).send('Invoice not found');
+        }
+
+        await invoice.destroy();
+
+        res.send('Invoice deleted successfully');
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+const deleteSubcontractor = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can delete subcontractors.');
+        }
+
+        const subcontractorId = req.params.id;
+        const subcontractor = await Subcontractor.findByPk(subcontractorId);
+
+        if (!subcontractor) {
+            return res.status(404).send('Subcontractor not found');
+        }
+
+        await subcontractor.destroy();
+
+        res.send('Subcontractor deleted successfully');
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        // Check if the user is trying to delete their own account
+        if (req.session.user.id === userId) {
+            return res.status(403).send('Access denied. You cannot delete your own account.');
+        }
+
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can delete users.');
+        }
+
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        await user.destroy();
+
+        res.send('User deleted successfully');
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+module.exports = {
+    renderAdminDashboard,
+    deleteInvoice,
+    deleteSubcontractor,
+    deleteUser,
+};
