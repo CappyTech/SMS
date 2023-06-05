332ea1ad9ac52f0cd454276341e56b53625da931
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-06-05 17:49:28 +0100

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,42 @@
+// controllers/index.js
+
 const packageJson = require('../package.json');
+const Invoice = require('../models/invoice');
+const Subcontractor = require('../models/subcontractor');
+const helpers = require('../helpers');
 
 const renderIndex = (req, res) => {
     res.render('index', {
         errorMessages: req.flash('error'),
         successMessage: req.flash('success'),
         session: req.session,
-        packageJson
+        packageJson,
+        message: req.query.message || '',
     });
 };
 
-const renderDashboard = (req, res) => {
-    if (req.session.user) {
-        res.render('dashboard', {
-            errorMessages: req.flash('error'),
-            successMessage: req.flash('success'),
-            session: req.session,
-            packageJson
-        });
-    } else {
-        res.redirect('/login');
+const renderDashboard = async (req, res) => {
+    try {
+        if (req.session.user) {
+
+            const subcontractors = await Subcontractor.findAll();
+            const invoices = await Invoice.findAll();
+
+            res.render('dashboard', {
+                errorMessages: req.flash('error'),
+                successMessage: req.flash('success'),
+                session: req.session,
+                packageJson,
+                message: req.query.message || '',
+                slimDateTime: helpers.slimDateTime,
+                Invoice: invoices,
+                Subcontractor: subcontractors,
+            });
+        } else {
+            res.redirect('/login');
+        }
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
     }
 };
 
