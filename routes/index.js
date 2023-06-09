0a640d7789e11330a0abf3eadbc70672a85f3fab
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-06-09 14:43:23 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,21 @@ const {
 router.get('/', renderIndex);
 router.get('/dashboard', renderDashboard);
 
+// Import the necessary controllers
+const {
+    getAccountPage
+} = require('../controllers/account');
+const {
+    getAccountSettingsPage,
+    updateAccountSettings
+} = require('../controllers/accountSettings');
+
+// Account page
+router.get('/account', getAccountPage);
+
+// Account settings
+router.get('/account/settings', getAccountSettingsPage);
+router.post('/account/settings', updateAccountSettings);
+
+
 module.exports = router;
