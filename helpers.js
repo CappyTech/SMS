3a9ce086776eaeedc9f3f2c5f8d071aa685ac72d
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-08-02 17:35:53 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -11,6 +11,19 @@ function slimDateTime(dateString) {
     return formattedDate.replace(/\//g, '/');
 }
 
+function formatCurrency(amount) {
+    // Check if the amount is a number
+    if (typeof amount !== 'number') {
+        throw new Error('Invalid input. Amount must be a number.');
+    }
+
+    // Format the amount with two decimal places and add £ symbol
+    const formattedAmount = '£' + amount.toFixed(2);
+
+    return formattedAmount;
+}
+
 module.exports = {
-    slimDateTime
+    slimDateTime,
+    formatCurrency,
 };
