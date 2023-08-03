fb7b1745c1c7ead4e019b888cd2eff8dd5076796
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-08-03 18:26:04 +0100

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,6 +6,24 @@ const Subcontractor = require('../models/subcontractor');
 const User = require('../models/user');
 const helpers = require('../helpers');
 
+
+// Render the create form for a user
+const renderUserCreateForm = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can create users.');
+        }
+
+        res.render('createUser', {
+            session: req.session,
+            packageJson,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
 // Render the admin dashboard
 const renderAdminDashboard = async (req, res) => {
     try {
@@ -187,11 +205,174 @@ const unassignUserFromSubcontractor = async (req, res) => {
     }
 };
 
+// Render the edit form for an invoice
+const renderInvoiceEditForm = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can edit invoices.');
+        }
+
+        const invoiceId = req.params.id;
+        const invoice = await Invoice.findByPk(invoiceId);
+
+        if (!invoice) {
+            return res.status(404).send('Invoice not found');
+        }
+
+        res.render('editInvoice', {
+            invoice,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// View an invoice
+const viewInvoice = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can view invoices.');
+        }
+
+        const invoiceId = req.params.id;
+        const invoice = await Invoice.findByPk(invoiceId);
+
+        if (!invoice) {
+            return res.status(404).send('Invoice not found');
+        }
+
+        res.render('viewInvoice', {
+            invoice,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// View a subcontractor
+const viewSubcontractor = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can view subcontractors.');
+        }
+
+        const subcontractorId = req.params.id;
+        const subcontractor = await Subcontractor.findByPk(subcontractorId);
+
+        if (!subcontractor) {
+            return res.status(404).send('Subcontractor not found');
+        }
+
+        res.render('viewSubcontractor', {
+            subcontractor,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// Render the edit form for a subcontractor
+const renderSubcontractorEditForm = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied.');
+        }
+
+        const subcontractorId = req.params.id;
+        const subcontractor = await Subcontractor.findByPk(subcontractorId);
+
+        if (!subcontractor) {
+            return res.status(404).send('Subcontractor not found');
+        }
+
+        res.render('editSubcontractor', {
+            subcontractor,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// View a user
+const viewUser = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can view users.');
+        }
+
+        const userId = req.params.id;
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        res.render('viewUser', {
+            user,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
+// Render the edit form for a user
+const renderUserEditForm = async (req, res) => {
+    try {
+        // Check if the user is an admin
+        if (req.session.user.role !== 'admin') {
+            return res.status(403).send('Access denied. Only admins can edit users.');
+        }
+
+        const userId = req.params.id;
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        res.render('editUser', {
+            user,
+            session: req.session,
+            packageJson,
+            slimDateTime: helpers.slimDateTime,
+        });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+};
+
 module.exports = {
+    renderUserCreateForm,
     renderAdminDashboard,
     deleteInvoice,
     deleteSubcontractor,
     deleteUser,
     assignUserToSubcontractor,
     unassignUserFromSubcontractor,
+    renderInvoiceEditForm,
+    viewInvoice,
+    viewSubcontractor,
+    renderSubcontractorEditForm,
+    viewUser,
+    renderUserEditForm,
 };
