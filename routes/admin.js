c78721c6aafcfb3a6aa69000d9c6858d83279699
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-08-23 20:19:22 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,8 @@ const {
     renderUserCreateForm,
     createUser,
     viewUser,
+    renderSubcontractorCreateForm,
+    createSubcontractor,
     viewSubcontractor,
     viewInvoice,
     renderUserEditForm,
@@ -25,26 +27,24 @@ const {
 
 // Render the admin dashboard
 router.get('/', renderAdminDashboard);
-
 // Handle user-subcontractor assignment
 router.post('/assign', assignUserToSubcontractor);
-
 // Handle user-subcontractor unassignment
 router.post('/unassign', unassignUserFromSubcontractor);
-
-// Handle the form submission for creating a new user
+// Render form new user
 router.get('/user/create', renderUserCreateForm);
-
 // Handle the form submission for creating a new user
 router.post('/user/submit', createUser);
-
 // View a user
 router.get('/user/view/:id', viewUser);
+// Render form new subcontractor
+router.get('/subcontractor/create', renderSubcontractorCreateForm);
+// Handle the form submission for creating a new subcontractor
+router.post('/subcontractor/submit', createSubcontractor);
 // View a subcontractor
 router.get('/subcontractor/view/:id', viewSubcontractor);
 // View an invoice
 router.get('/invoice/view/:id', viewInvoice);
-
 // Render the edit form for a user
 router.get('/user/edit/:id', renderUserEditForm);
 router.post('/user/update/:id', updateUser);
@@ -54,7 +54,6 @@ router.post('/subcontractor/update/:id', updateSubcontractor);
 // Render the edit form for an invoice
 router.get('/invoice/edit/:id', renderInvoiceEditForm);
 router.post('/invoice/update/:id', updateInvoice);
-
 // Delete a user
 router.post('/user/delete/:id', deleteUser);
 // Delete a subcontractor
