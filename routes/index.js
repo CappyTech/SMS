7802771a3afab3e80265ec9335fb99977af9bbd8
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-10-27 15:39:26 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,16 @@ router.post('/account/settings', updateAccountSettings);
 const {
     renderFilteredMonthlyReturns,
     renderMonthlyReturnsForm,
+    renderMonthlyReturns,
 } = require('../controllers/monthlyReturns');
 
 router.get('/monthly/returns', renderFilteredMonthlyReturns);
 router.get('/monthly/returns/form', renderMonthlyReturnsForm);
+router.get('/monthly/returns/:month/:year/:subcontractor', renderMonthlyReturns);
 
+const {
+    renderYearlyReturns
+} = require('../controllers/yearlyReturns');
+
+router.get('/yearly/returns/:year/:id', renderYearlyReturns);
 module.exports = router;
