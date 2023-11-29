9b5203e4db836f34395cba3eea4d21b58afedb07
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-11-29 01:35:51 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -18,9 +18,7 @@ function formatCurrency(amount) {
     }
 
     // Format the amount with two decimal places and add £ symbol
-    const formattedAmount = '£' + amount.toFixed(2);
-
-    return formattedAmount;
+    return '£' + amount.toFixed(2);
 }
 
 const isAdmin = (req, res, next) => {
@@ -30,8 +28,77 @@ const isAdmin = (req, res, next) => {
     next();
   };
 
+
+function validateInvoiceData(data) {
+    const errors = [];
+
+    // Define required fields and their validation rules
+    const validations = {
+        invoiceNumber: value => value && value.trim() !== '',
+        kashflowNumber: value => value && value.trim() !== '',
+        invoiceDate: value => value && !isNaN(Date.parse(value)),
+        remittanceDate: value => !value || !isNaN(Date.parse(value)),
+        labourCost: value => !isNaN(value) && Number(value) >= 0,
+        materialCost: value => !isNaN(value) && Number(value) >= 0,
+        month: value => !isNaN(value) && Number(value) >= 1 && Number(value) <= 12,
+        year: value => !isNaN(value) && Number(value) >= 2000 // Assuming year 2000 as the earliest acceptable year
+    };
+
+    // Perform the validations
+    Object.keys(validations).forEach(field => {
+        if (!validations[field](data[field])) {
+            errors.push(`Invalid or missing value for ${field}`);
+        }
+    });
+
+    // Special handling for submissionDate
+    if (data.submissionDate === '0000-00-00 00:00:00' || !data.submissionDate) {
+        data.submissionDate = null;
+    }
+
+    if (errors.length > 0) {
+        throw new Error(errors.join(', '));
+    }
+
+    return data; // Return the validated data
+}
+
+
+function calculateInvoiceAmounts(labourCost, materialCost, isGross, cisNumber, vatNumber) {
+    labourCost = parseFloat(labourCost);
+    materialCost = parseFloat(materialCost);
+
+    const grossAmount = labourCost + materialCost;
+    let cisRate, reverseCharge;
+
+    // Determine the CIS rate based on the subcontractor's status
+    if (isGross) {
+        cisRate = 0.0; // 0% for gross subcontractors
+    } else if (cisNumber) {
+        cisRate = 0.2; // 20% for subcontractors with a CIS number
+    } else {
+        cisRate = 0.3; // 30% if CIS number is null
+    }
+
+    const cisAmount = labourCost * cisRate;
+    const netAmount = grossAmount - cisAmount;
+
+    // Calculate Reverse Charge only if VAT number is present
+    reverseCharge = vatNumber ? grossAmount * 0.2 : 0; // 20% Reverse Charge
+
+    return {
+        grossAmount: grossAmount.toFixed(2),
+        cisAmount: cisAmount.toFixed(2),
+        netAmount: netAmount.toFixed(2),
+        reverseCharge: reverseCharge.toFixed(2)
+    };
+}
+
+
 module.exports = {
     slimDateTime,
     formatCurrency,
-    isAdmin
+    isAdmin,
+    validateInvoiceData,
+    calculateInvoiceAmounts
 };
