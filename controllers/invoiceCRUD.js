805be5b15d5246d454ed37f1ae6e066b37b30a67
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-11-29 01:35:33 +0000

    Update invoiceCRUD.js

--- controllers/invoiceCRUD.js
+++ controllers/invoiceCRUD.js
@@ -4,156 +4,31 @@ const express = require('express');
 const router = express.Router();
 
 const packageJson = require('../package.json');
-const User = require('../models/user');
 const Invoice = require('../models/invoice');
 const Subcontractor = require('../models/subcontractor');
-const helpers = require('../helpers');
-
-const {
-    Op
-} = require('sequelize');
-
+const {validateInvoiceData,calculateInvoiceAmounts, slimDateTime, formatCurrency} = require('../helpers');
 const createInvoice = async (req, res) => {
     try {
-        const {
-            invoiceNumber,
-            kashflowNumber,
-            invoiceDate,
-            remittanceDate,
-            labourCost,
-            materialCost,
-            submissionDate,
-            month,
-            year
-        } = req.body;
-        console.log(req.body);
+        const validatedData = validateInvoiceData(req.body);
+        const amounts = calculateInvoiceAmounts(validatedData.labourCost, validatedData.materialCost, req.params.id); // Adjust parameters as needed
 
-        const subcontractor = await Subcontractor.findByPk(req.params.selected);
-
-        if (!subcontractor) {
-            console.error('Subcontractor not found');
-            req.flash('error', 'Subcontractor not found');
-            const referrer = req.get('referer') || '/';
-            res.redirect(referrer);
-        }
-
-        if (submissionDate === '0000-00-00 00:00:00') {
-            if (subcontractor.isGross) {
-                const cisAmount = 0;
-                const netAmount = parseInt(labourCost) - parseInt(cisAmount) + parseInt(materialCost);
-                const grossAmount = parseInt(labourCost) + parseInt(materialCost);
-                const reverseCharge = parseInt(grossAmount) * 0.2;
-
-                const invoice = await Invoice.create({
-                    invoiceNumber,
-                    kashflowNumber,
-                    invoiceDate,
-                    remittanceDate,
-                    grossAmount,
-                    labourCost,
-                    materialCost,
-                    cisAmount,
-                    netAmount,
-                    submissionDate,
-                    reverseCharge,
-                    month,
-                    year,
-                    SubcontractorId: subcontractor.id,
-                });
-
-                await subcontractor.addInvoice(invoice);
-                console.log('Invoice was created with submission as null');
-                req.flash('success', 'Invoice created successfully');
-                const referrer = req.get('referer') || '/';
-                res.redirect(referrer);
-            } else {
-                const cisAmount = parseInt(labourCost) * 0.2;
-                const netAmount = parseInt(labourCost) - parseInt(cisAmount) + parseInt(materialCost);
-                const grossAmount = parseInt(labourCost) + parseInt(materialCost);
-                const reverseCharge = parseInt(grossAmount) * 0.2;
-
-                const invoice = await Invoice.create({
-                    invoiceNumber,
-                    kashflowNumber,
-                    invoiceDate,
-                    remittanceDate,
-                    grossAmount,
-                    labourCost,
-                    materialCost,
-                    cisAmount,
-                    netAmount,
-                    submissionDate: null,
-                    reverseCharge,
-                    month,
-                    year,
-                    SubcontractorId: subcontractor.id,
-                });
-
-                await subcontractor.addInvoice(invoice);
-                console.log('Invoice was created with submission as null');
-                req.flash('success', 'Invoice created successfully');
-                const referrer = req.get('referer') || '/';
-                res.redirect(referrer);
-            }
-        } else {
-            if (subcontractor.isGross) {
-                const cisAmount = 0;
-                const netAmount = parseInt(labourCost) - parseInt(cisAmount) + parseInt(materialCost);
-                const grossAmount = parseInt(labourCost) + parseInt(materialCost);
-                const reverseCharge = parseInt(grossAmount) * 0.2;
-
-                const invoice = await Invoice.create({
-                    invoiceNumber,
-                    kashflowNumber,
-                    invoiceDate,
-                    remittanceDate,
-                    grossAmount,
-                    labourCost,
-                    materialCost,
-                    cisAmount,
-                    netAmount,
-                    submissionDate,
-                    reverseCharge,
-                    month,
-                    year,
-                    SubcontractorId: subcontractor.id,
-                });
-
-                await subcontractor.addInvoice(invoice);
-                console.log('Invoice was NOT created with submission as null');
-                req.flash('success', 'Invoice created successfully');
-                const referrer = req.get('referer') || '/';
-                res.redirect(referrer);
-            } else {
-                const cisAmount = parseInt(labourCost) * 0.2;
-                const netAmount = parseInt(labourCost) - parseInt(cisAmount) + parseInt(materialCost);
-                const grossAmount = parseInt(labourCost) + parseInt(materialCost);
-                const reverseCharge = parseInt(grossAmount) * 0.2;
-
-                const invoice = await Invoice.create({
-                    invoiceNumber,
-                    kashflowNumber,
-                    invoiceDate,
-                    remittanceDate,
-                    grossAmount,
-                    labourCost,
-                    materialCost,
-                    cisAmount,
-                    netAmount,
-                    submissionDate,
-                    reverseCharge,
-                    month,
-                    year,
-                    SubcontractorId: subcontractor.id,
-                });
-
-                await subcontractor.addInvoice(invoice);
-                console.log('Invoice was NOT created with submission as null');
-                req.flash('success', 'Invoice created successfully');
-                const referrer = req.get('referer') || '/';
-                res.redirect(referrer);
-            }
-        }
+        // Create invoice record
+        await Invoice.create({
+            invoiceNumber: validatedData.invoiceNumber,
+            kashflowNumber: validatedData.kashflowNumber,
+            invoiceDate: validatedData.invoiceDate,
+            remittanceDate: validatedData.remittanceDate,
+            grossAmount: amounts.grossAmount,
+            labourCost: validatedData.labourCost,
+            materialCost: validatedData.materialCost,
+            cisAmount: amounts.cisAmount,
+            netAmount: amounts.netAmount,
+            submissionDate: validatedData.submissionDate,
+            reverseCharge: amounts.reverseCharge,
+            month: validatedData.month,
+            year: validatedData.year,
+            SubcontractorId: req.params.id
+        });
     } catch (error) {
         if (error.name === 'SequelizeValidationError') {
             const errorMessages = error.errors.map((err) => err.message);
@@ -172,7 +47,7 @@ const createInvoice = async (req, res) => {
         res.redirect(referrer);
     }
 };
-const viewInvoice = async (req, res) => {
+const readInvoice = async (req, res) => {
     try {
         // Check if the user is an admin
         if (req.session.user.role !== 'admin') {
@@ -190,8 +65,8 @@ const viewInvoice = async (req, res) => {
             invoice,
             session: req.session,
             packageJson,
-            slimDateTime: helpers.slimDateTime,
-            formatCurrency: helpers.formatCurrency,
+            slimDateTime: slimDateTime,
+            formatCurrency: formatCurrency,
         });
     } catch (error) {
         console.error('Error viewing invoice:', error);
@@ -199,46 +74,36 @@ const viewInvoice = async (req, res) => {
     }
 };
 const updateInvoice = async (req, res) => {
-    let message = 'Invoice updated.';
-    let status = 200;
-
     try {
         validateInvoiceData(req.body);
 
         const invoice = await Invoice.findByPk(req.params.id);
         if (!invoice) {
-            message = 'Invoice not found.';
-            status = 404;
-            throw new Error(message);
+            throw new Error('Invoice not found');
         }
 
-        const subcontractor = await Subcontractor.findByPk(invoice.SubcontractorId);
+        if (!invoice.subcontractorId) {
+            throw new Error(`No subcontractorId associated with invoice ID: ${req.params.id}`);
+        }
+
+        const subcontractor = await Subcontractor.findByPk(invoice.subcontractorId);
         if (!subcontractor) {
-            message = `Subcontractor not found for the invoice with ID: ${req.params.id}`;
-            status = 404;
-            throw new Error(message);
+            throw new Error(`Subcontractor with ID: ${invoice.subcontractorId} not found for invoice ${req.params.id}`);
         }
 
         const amounts = calculateInvoiceAmounts(req.body.labourCost, req.body.materialCost, req.body.submissionDate, subcontractor.isGross);
 
-        await Invoice.update({
-            ...req.body,
-            ...amounts
-        }, {
-            where: {
-                id: req.params.id
-            }
-        });
+        await Invoice.update({ ...req.body, ...amounts }, { where: { id: req.params.id } });
 
-        req.flash('success', message);
+        req.flash('success', 'Invoice updated successfully');
+        return res.redirect('/admin'); // Ensure to return here
     } catch (error) {
         console.error('Error updating invoice:', error.message);
         req.flash('error', `Error updating invoice with ID: ${req.params.id}. Details: ${error.message}`);
-        status = 500;
+        return res.redirect(req.get('referer') || '/'); // Ensure to return here
     }
-
-    return res.status(status).redirect('/admin');
 };
+
 const deleteInvoice = async (req, res) => {
     try {
         // Check if the user is an admin
@@ -265,8 +130,8 @@ const deleteInvoice = async (req, res) => {
     }
 };
 
-router.post('/invoice/submit/:selected', createInvoice);
-router.get('/invoice/view/:id', viewInvoice);
+router.post('/invoice/create/:selected', createInvoice);
+router.get('/invoice/read/:id', readInvoice);
 router.post('/invoice/update/:id', updateInvoice);
 router.get('/invoice/delete/:id', deleteInvoice);
 
