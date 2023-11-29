d64383e593f5370e5263dd86cc223da2c1420505
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-11-29 20:30:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -230,6 +230,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000, 'localhost', () => {
-    console.log('Server listening on http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server listening...');
 });
