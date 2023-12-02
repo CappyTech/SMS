b68c83a123e7bfa98a3d40d5e23499c8b4c32c93
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-12-02 19:04:25 +0000

    Update app.js

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, '0.0.0.0', () => {
     console.log('Server listening...');
 });
