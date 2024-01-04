0801bdfa95e5d7e9ef43db2c309305e13314f0d9
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-01-04 08:39:33 +0000

    Update db.js

--- db.js
+++ db.js
@@ -5,13 +5,15 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false
+    logging: process.env.DEBUG ? console.log : false,
 });
 
 sequelize
     .authenticate()
     .then(() => {
-        console.log('Database connection has been established successfully.');
+        if (process.env.DEBUG) {
+            console.log('Database connection has been established successfully.');
+        }
     })
     .catch((error) => {
         console.error('Unable to connect to the database:', error);
