e7b1507e9310664d01aba803296e704344c1b47f
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-07-26 16:52:02 +0100

    migrations

--- migrations/20230602161049-make-reverse-charge-null.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Invoices', 'reverseChargeAmount');
-  },
-  up: async (queryInterface, Sequelize) => {},
-};
