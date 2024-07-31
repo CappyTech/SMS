9a4879c2f0526c8409174bd6161e612314121fc8
HeronCS <86607231+HeronCS@users.noreply.github.com>
2024-07-31 17:03:25 +0100

    Update quoteCRUD.js

--- controllers/quoteCRUD.js
+++ controllers/quoteCRUD.js
@@ -2,26 +2,28 @@
 const express = require('express');
 const router = express.Router();
 const packageJson = require('../package.json');
-const Quote = require('../models/quote');
+const Quotes = require('../models/quote');
+const Clients = require('../models/client');
 const helpers = require('../helpers');
 const moment = require('moment');
 const logger = require('../logger');
 
 const createQuote = async (req, res) => {
     try {
-        const { date, job_ref, site, client, contact_ref, value, desc, invoice_no, invoice_date, po_number } = req.body;
-
-        const newQuote = await Quote.create({
-            date,
-            job_ref,
-            site,
-            client,
-            contact_ref,
-            value,
-            desc,
-            invoice_no,
-            invoice_date,
-            po_number
+        const { date, quote_ref, job_ref, location, contact_ref, value, desc, invoice_no, invoice_date} = req.body;
+        logger.info(req.body);
+        const clientId = req.params.client;
+        const newQuote = await Quotes.create({
+            date: date,
+            quote_ref: quote_ref,
+            job_ref: job_ref,
+            location: location,
+            clientId: clientId,
+            contact_ref: contact_ref,
+            value: value,
+            desc: desc,
+            invoice_no: invoice_no,
+            invoice_date: invoice_date
         });
 
         res.redirect(`/quote/read/${newQuote.id}`);
@@ -29,12 +31,6 @@ const createQuote = async (req, res) => {
         if (error.name === 'SequelizeValidationError') {
             const errorMessages = error.errors.map((err) => err.message);
             logger.error(`Validation errors: ${errorMessages.join(', ')}`);
-            return res.render('createQuote', {
-                errorMessages: req.flash('error'),
-                successMessage: req.flash('success'),
-                session: req.session,
-                packageJson,
-            });
         }
         logger.error(`Error creating quote: ${error.message}`);
         req.flash('error', 'Error creating quote: ' + error.message);
@@ -49,20 +45,25 @@ const readQuote = async (req, res) => {
             return res.redirect('/');
         }
 
-        const quote = await Quote.findByPk(req.params.id);
+        const quote = await Quotes.findByPk(req.params.quote, {
+            include: [{
+                model: Clients
+            }]
+          });
 
-        if (!quote) {
+        if (!req.params.quote) {
             return res.status(404).json({ error: 'Quote not found' });
+        } else {
+            res.render('viewQuote', {
+                quote,
+                errorMessages: req.flash('error'),
+                successMessage: req.flash('success'),
+                session: req.session,
+                packageJson,
+                moment: moment,
+                slimDateTime: helpers.slimDateTime,
+            });
         }
-
-        res.render('viewQuote', {
-            quote,
-            errorMessages: req.flash('error'),
-            successMessage: req.flash('success'),
-            session: req.session,
-            packageJson,
-            moment: moment,
-        });
     } catch (error) {
         logger.error(`Error viewing quote: ${error.message}`);
         req.flash('error', 'Error viewing quote: ' + error.message);
@@ -76,8 +77,9 @@ const readQuotes = async (req, res) => {
             req.flash('error', 'Access denied.');
             return res.redirect('/');
         }
-
-        const quotes = await Quote.findAll({
+        
+        const quotes = await Quotes.findAll({
+            where: { ClientId: req.params.client },
             order: [['date', 'DESC']],
         });
 
@@ -88,6 +90,7 @@ const readQuotes = async (req, res) => {
             session: req.session,
             packageJson,
             moment: moment,
+            slimDateTime: helpers.slimDateTime,
         });
     } catch (error) {
         logger.error(`Error viewing quotes: ${error.message}`);
@@ -99,12 +102,12 @@ const readQuotes = async (req, res) => {
 const updateQuote = async (req, res) => {
     try {
 
-        const quote = await Quote.findByPk(req.params.id);
+        const quote = await Quotes.findByPk(req.params.id);
         if (!quote) {
             throw new Error('Quote not found');
         }
 
-        await Quote.update(req.body, { where: { id: req.params.id } });
+        await Quotes.update(req.body, { where: { id: req.params.id } });
 
         req.flash('success', 'Quote updated successfully');
         return res.redirect(`/quote/read/${req.params.id}`);
@@ -121,18 +124,17 @@ const deleteQuote = async (req, res) => {
             return res.status(403).send('Access denied. Only admins can delete quotes.');
         }
 
-        const quote = await Quote.findByPk(req.params.id);
+        const quote = await Quotes.findByPk(req.params.id);
 
         if (!quote) {
             req.flash('error', 'Quote not found');
-            return res.redirect(req.get('referer') || '/dashboard');
+            return res.redirect('/dashboard/quote');
         }
 
         await quote.destroy();
 
         req.flash('success', 'Quote deleted successfully');
-        const referer = req.get('referer') || '/dashboard';
-        res.redirect(referer);
+        res.redirect('/dashboard/quote');
     } catch (error) {
         logger.error(`Error deleting quote: ${error.message}`);
         req.flash('error', 'Error deleting quote: ' + error.message);
@@ -140,7 +142,7 @@ const deleteQuote = async (req, res) => {
     }
 };
 
-router.post('/quote/create/', createQuote);
+router.post('/quote/create/:client', createQuote);
 router.get('/quote/read/:quote', readQuote);
 router.get('/quote/read/:client', readQuotes);
 router.post('/quote/update/:id', updateQuote);
