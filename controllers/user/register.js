e91331ee363a113ff1b8c01aa5e4e1a1da1ff477
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-08-14 20:16:22 +0100

    Update register.js

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const packageJson = require("../../package.json");
-const speakeasy = require("speakeasy");
 const User = require("../../models/user");
 const { Op } = require("sequelize");
 const logger = require('../../logger'); 
@@ -12,15 +10,12 @@ const renderRegistrationForm = (req, res) => {
     res.render(path.join('user', 'register'), {
         errorMessages: req.flash('error'),
         successMessage: req.flash('success'),
-        session: req.session,
-        
         message: req.query.message || '',
     });
 };
 
 const registerUser = async (req, res) => {
     try {
-        const secret = speakeasy.generateSecret({ length: 20 });
         const { username, email, password } = req.body;
 
         // Check if the email or username already exists
