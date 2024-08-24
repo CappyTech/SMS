cdf177f546e4fd6d21090c972a1b598e68a8d9f9
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-08-24 19:53:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -73,14 +73,33 @@ app.use(
     helmet.contentSecurityPolicy({
         directives: {
             defaultSrc: ["'self'", "https://sms.heroncs.co.uk"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com"],
-            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com", "https://unpkg.com"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://unpkg.com"],
             fontSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.gstatic.com", "https://fonts.googleapis.com"],
-            imgSrc: ["'self'", "data:", "otpauth:", "https://i.creativecommons.org", "https://licensebuttons.net", "https://sms-heroncs.co.uk"],
-            connectSrc: ["'self'", "https://sms.heroncs.co.uk"],
+            imgSrc: [
+                "'self'",
+                "data:",
+                "otpauth:",
+                "https://i.creativecommons.org",
+                "https://licensebuttons.net",
+                "https://sms.heroncs.co.uk",
+                "https://a.tile.openstreetmap.org",
+                "https://b.tile.openstreetmap.org",
+                "https://c.tile.openstreetmap.org",
+                "https://unpkg.com"
+            ],
+            connectSrc: [
+                "'self'",
+                "https://sms.heroncs.co.uk",
+                "https://nominatim.openstreetmap.org",
+                "https://api.openstreetmap.org"
+            ]
         },
     })
 );
+
+
+
 
 
 const sessionStore = new MySQLStore({
