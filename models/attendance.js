0b4b9f3d65ad57123f44c7992864bd7b5f57c4c6
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-09-01 03:49:14 +0100

    Update attendance.js

--- models/attendance.js
+++ models/attendance.js
@@ -2,6 +2,8 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db.js');
 const Locations = require('./location');
+const Employees = require('./employee');
+const Subcontractors = require('./subcontractor');
 
 const Attendances = sequelize.define('Attendances', {
     id: {
@@ -22,6 +24,26 @@ const Attendances = sequelize.define('Attendances', {
             key: 'id',
         },
         onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    },
+    employeeId: {
+        type: DataTypes.UUID,
+        allowNull: true, // Make it nullable so it can be either employee or subcontractor
+        references: {
+            model: Employees,
+            key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+    },
+    subcontractorId: {
+        type: DataTypes.UUID,
+        allowNull: true, // Make it nullable so it can be either employee or subcontractor
+        references: {
+            model: Subcontractors,
+            key: 'id',
+        },
+        onDelete: 'SET NULL',
         onUpdate: 'CASCADE',
     },
     holidays_taken: {
