8ea3e1e58c2c9beb418253281f62ba0f7c744fc8
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-09-02 19:51:42 +0100

    Update invoice.js

--- controllers/forms/invoice.js
+++ controllers/forms/invoice.js
@@ -29,22 +29,6 @@ const renderInvoiceCreateForm = async (req, res) => {
     }
 };
 
-// Assuming this is used as middleware to fetch subcontractor details dynamically
-const getSubcontractorDetails = async (req, res) => {
-    try {
-        const subcontractor = await Subcontractor.findByPk(req.params.subcontractor);
-
-        if (!subcontractor) {
-            return res.status(404).json({ error: 'Subcontractor not found' });
-        }
-        logger.info(`Fetched subcontractor details: ${JSON.stringify(subcontractor)}`);
-        res.json(subcontractor);
-    } catch (error) {
-        logger.error('Error fetching subcontractor details: ' + error.message);
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-};
-
 const renderInvoiceUpdateForm = async (req, res) => {
     try {
         if (!req.session.user || req.session.user.role !== 'admin') {
@@ -76,7 +60,6 @@ const renderInvoiceUpdateForm = async (req, res) => {
 };
 
 router.get('/invoice/create/', renderInvoiceCreateForm);
-router.get('/fetch/subcontractor/:subcontractor', getSubcontractorDetails)
 router.get('/invoice/update/:invoice', renderInvoiceUpdateForm);
 
 module.exports = router;
