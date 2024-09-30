0c9e234890e9aba2ef185ce1bded311e40ef7d61
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-09-30 01:08:21 +0100

    Update employeeCRUD.js

--- controllers/CRUD/employeeCRUD.js
+++ controllers/CRUD/employeeCRUD.js
@@ -10,8 +10,8 @@ const Employees = require('../../models/employee');
 // Create Employee
 const createEmployee = async (req, res) => {
     try {
-        const { name, email, phoneNumber, position, type, status, contactName, contactNumber } = req.body;
-        await Employees.create({ name, email, phoneNumber, position, type, status, contactName, contactNumber });
+        const { name, email, phoneNumber, position, type, status, contactName, contactNumber, hourlyRate } = req.body;
+        await Employees.create({ name, email, phoneNumber, position, type, status, contactName, contactNumber, hourlyRate });
         req.flash('success', 'Employee created successfully.');
         res.redirect('/dashboard/employee');
     } catch (error) {
@@ -47,8 +47,8 @@ const readEmployee = async (req, res) => {
 // Update Employee
 const updateEmployee = async (req, res) => {
     try {
-        const { name, email, phoneNumber, position, type, status, contactName, contactNumber } = req.body;
-        await Employees.update({ name, email, phoneNumber, position, type, status, contactName, contactNumber }, { where: { id: req.params.employee } });
+        const { name, email, phoneNumber, position, type, status, contactName, contactNumber, hourlyRate } = req.body;
+        await Employees.update({ name, email, phoneNumber, position, type, status, contactName, contactNumber, hourlyRate }, { where: { id: req.params.employee } });
         req.flash('success', 'Employee updated successfully.');
         res.redirect('/dashboard/employee');
     } catch (error) {
