c22c77861c2d7fa7ecbd7d792f131977c3db5c3f
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 15:10:18 +0100

    Update user.js

--- controllers/forms/user.js
+++ controllers/forms/user.js
@@ -3,22 +3,14 @@ const router = express.Router();
 const helpers = require('../../helpers');
 const logger = require('../../logger');
 const path = require('path');
-
 const User = require('../../models/user');
 
 const renderUserCreateForm = async (req, res) => {
     try {
-        if (!req.session.user || req.session.user.role !== 'admin') {
-            req.flash('error', 'Access denied.');
-            return res.redirect('/');
-        }
-
         res.render(path.join('users', 'createUser'), {
             title: 'Create User',
             errorMessages: req.flash('error'),
             successMessage: req.flash('success'),
-            
-            
         });
     } catch (error) {
         logger.error('Error rendering user create form:' + error.message);
@@ -29,25 +21,16 @@ const renderUserCreateForm = async (req, res) => {
 
 const renderUserUpdateForm = async (req, res) => {
     try {
-        if (!req.session.user || req.session.user.role !== 'admin') {
-            req.flash('error', 'Access denied.');
-            return res.redirect('/');
-        }
-
         const userId = req.params.user;
         const user = await User.findByPk(userId);
-
         if (!user) {
             return res.status(404).send('User not found');
         }
-
         res.render(path.join('users', 'updateUser'), {
             title: 'Update User',
             user,
             errorMessages: req.flash('error'),
             successMessage: req.flash('success'),
-            
-            
             slimDateTime: helpers.slimDateTime,
             formatCurrency: helpers.formatCurrency,
         });
