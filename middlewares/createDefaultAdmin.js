ca57ca4ae988294efbeeb22e2d8fb41668a066bc
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 15:10:24 +0100

    Create createDefaultAdmin.js

---
+++ middlewares/createDefaultAdmin.js
@@ -0,0 +1,38 @@
+const { Op } = require("sequelize");
+const Users = require('../models/Users'); // Adjust the path as necessary
+const logger = require('../logger');
+
+const createDefaultAdmin = async (req, res, next) => {
+    try {
+        const admin = await Users.findOne({
+            where: {
+                [Op.or]: [
+                    { username: 'admin' },
+                    { role: 'admin' }
+                ]
+            }
+        });
+        if (!admin) {
+            await Users.create({
+                username: process.env.ADMIN_USERNAME,
+                email: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PASSWORD,
+                role: 'admin',
+            }, {
+                fields: ['username', 'email', 'password', 'role']
+            });
+            if (process.env.DEBUG) {
+                logger.info('Default admin created.');
+            }
+        } else {
+            if (process.env.DEBUG) {
+                logger.info('Default admin already exists.');
+            }
+        }
+    } catch (error) {
+        logger.error('Error creating default admin: ' + error);
+    }
+    next();
+};
+
+module.exports = createDefaultAdmin;
