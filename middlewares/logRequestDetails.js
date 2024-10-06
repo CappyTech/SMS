e23c9782f236bf55e71003ce1c2cab7b2699ab30
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 15:10:27 +0100

    Create logRequestDetails.js

---
+++ middlewares/logRequestDetails.js
@@ -0,0 +1,27 @@
+const useragent = require('useragent');
+const logger = require('../logger');
+
+const logRequestDetails = (req, res, next) => {
+    const agent = req.headers['user-agent'] ? useragent.parse(req.headers['user-agent']) : null;
+
+    const logData = {
+        method: req.method,
+        url: req.originalUrl,
+        ip: req.ip,
+        userAgent: agent ? {
+            browser: agent.toAgent(),
+            os: agent.os.toString(),
+        } : 'Unknown',
+        headers: req.headers,
+        referer: req.headers['referer'] || 'N/A',
+        host: req.headers['host'] || 'N/A',
+        xForwardedFor: req.headers['x-forwarded-for'] || 'N/A',
+        timestamp: new Date().toISOString(),
+    };
+
+    logger.info('Incoming Request Details: ' + JSON.stringify(logData, null, 2));
+
+    next();
+};
+
+module.exports = logRequestDetails;
