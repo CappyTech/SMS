303914112904827304b0c1ab5d57735dad2187b5
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 23:04:58 +0100

    Update security.js

--- middlewares/security.js
+++ middlewares/security.js
@@ -3,6 +3,32 @@ const xss = require('xss-clean');
 
 const securityMiddleware = [
     helmet(),
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'", "https://app.heroncs.co.uk"],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com", "https://unpkg.com"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://unpkg.com"],
+            fontSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.gstatic.com", "https://fonts.googleapis.com"],
+            imgSrc: [
+                "'self'",
+                "data:",
+                "otpauth:",
+                "https://i.creativecommons.org",
+                "https://licensebuttons.net",
+                "https://sms.heroncs.co.uk",
+                "https://a.tile.openstreetmap.org",
+                "https://b.tile.openstreetmap.org",
+                "https://c.tile.openstreetmap.org",
+                "https://unpkg.com"
+            ],
+            connectSrc: [
+                "'self'",
+                "https://app.heroncs.co.uk",
+                "https://nominatim.openstreetmap.org",
+                "https://api.openstreetmap.org"
+            ]
+        },
+    }),
     xss()
 ];
 
