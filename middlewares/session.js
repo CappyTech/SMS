8cfbf506fd76ce9ec93102f11c9ad0213a3b5913
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 23:05:01 +0100

    Update session.js

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,13 +1,29 @@
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 require('dotenv').config();
+const logger = require('../services/loggerService');
 
 const options = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    clearExpired: true,
+    checkExpirationInterval: 300000,
+    expiration: 28800000,
+    createDatabaseTable: true,
+    endConnectionOnClose: true,
+    disableTouch: false,
+    charset: 'utf8mb4_bin',
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
 };
 
 const sessionStore = new MySQLStore(options);
@@ -18,7 +34,18 @@ const sessionMiddleware = session({
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === 'production' }
+    cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 28800000,
+        sameSite: 'strict',
+    }
+});
+
+sessionStore.onReady().then(() => {
+    logger.info('MySQLStore ready');
+}).catch(error => {
+    logger.error(error);
 });
 
 module.exports = sessionMiddleware;
