415579ba3d25ba3d96302e8453625708f76a8be4
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-10-06 15:10:38 +0100

    Create syncDatabase.js

---
+++ middlewares/syncDatabase.js
@@ -0,0 +1,48 @@
+const { Op } = require("sequelize");
+const sequelize = require('../db'); // Adjust the path as necessary
+const Users = require('../models/Users'); // Adjust the path as necessary
+const logger = require('../logger');
+
+const syncDatabase = async (req, res, next) => {
+    try {
+        // Check for existing tables
+        const tables = await sequelize.getQueryInterface().showAllTables();
+        if (tables.length === 0) {
+            logger.info('The database has no tables.');
+            await sequelize.sync();
+            logger.info('All models synchronized.');
+            const admin = await Users.findOne({
+                where: {
+                    [Op.or]: [
+                        { username: 'admin' },
+                        { role: 'admin' }
+                    ]
+                }
+            });
+            if (!admin) {
+                await Users.create({
+                    username: process.env.ADMIN_USERNAME,
+                    email: process.env.ADMIN_EMAIL,
+                    password: process.env.ADMIN_PASSWORD,
+                    role: 'admin',
+                }, {
+                    fields: ['username', 'email', 'password', 'role']
+                });
+                if (process.env.DEBUG) {
+                    logger.info('Default admin created.');
+                }
+            } else {
+                if (process.env.DEBUG) {
+                    logger.info('Default admin already exists.');
+                }
+            }
+        } else {
+            logger.info(`The database has the following tables: ${tables.join(', ')}`);
+        }
+    } catch (error) {
+        logger.error('Error fetching tables / syncing: ' + error);
+    }
+    next();
+};
+
+module.exports = syncDatabase;
