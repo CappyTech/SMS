c7580a6b6f2d06bfe1d8bbf0902c04f3946711cf
HeronCS <86607231+HeronCS@users.noreply.github.com>
2024-10-08 14:34:09 +0100

    Update rateLimiter.js

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 1500, // limit each IP to 1500 requests per windowMs
     message: 'Too many requests from this IP, please try again later.'
 });
 
