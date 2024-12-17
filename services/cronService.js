557e09218db3af59cfee7c9f4628eca4810eacd5
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-12-17 05:08:47 +0000

    Update cronService.js

--- services/cronService.js
+++ services/cronService.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const fetchKashFlowData2 = require('../kf/fetchKashFlowData2');
+const fetchKF = require('../kf/fetchKashFlowData');
 const logger = require('./loggerService');
 const schedule = '0 6-18 * * *'; // Every hour from 6 AM to 6 PM
 
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
     cron.schedule(schedule, async () => {
         try {
             logger.info('Cron job started: Fetching KashFlow data...');
-            await fetchKashFlowData2.fetchKashFlowData2();
+            await fetchKF.fetchKashFlowData();
             logger.info('Cron job completed: KashFlow data fetched successfully.');
         } catch (error) {
             logger.error('Cron job failed: ' + error.message);
