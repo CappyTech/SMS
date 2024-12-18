8e26199748ff40f09de0b68e7f23745f8bbfa46d
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-12-18 19:37:42 +0000

    Update sequelizeDatabaseService.js

--- services/sequelizeDatabaseService.js
+++ services/sequelizeDatabaseService.js
@@ -16,7 +16,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        logging: false,
+        logging: false,  // Set to console.log for query debugging
     }
 );
 
@@ -31,27 +31,27 @@ sequelize
     .catch((error) => {
         logger.error('Unable to connect to the MySQL database: ' + error.message);
         logger.error('Details:', error);
+        process.exit(1);
     });
 
-// Dynamically load all models from the models directory
+// Step 1: Dynamically load all models from the models directory
 const modelsDirectory = path.join(__dirname, '../models/sequelize');
-fs.readdirSync(modelsDirectory)
-    .filter((file) => {
-        return (
-            file.indexOf('.') !== 0 &&
-            file !== basename &&
-            file.slice(-3) === '.js' &&
-            file.indexOf('.test.js') === -1
-        );
-    })
-    .forEach((file) => {
-        const model = require(path.join(modelsDirectory, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
 
-// Set up model associations
-Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
+// Load model files
+const modelFiles = fs
+    .readdirSync(modelsDirectory)
+    .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js');
+
+// Define models (Step 1)
+modelFiles.forEach((file) => {
+    const model = require(path.join(modelsDirectory, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+});
+
+// Step 2: Set up associations (after all models are defined)
+modelFiles.forEach((file) => {
+    const modelName = path.basename(file, '.js');
+    if (db[modelName] && db[modelName].associate) {
         db[modelName].associate(db);
     }
 });
