186d5d19e827086808e4153f34b4c7b9964841c6
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-12-18 19:37:28 +0000

    Update client.js

--- models/sequelize/client.js
+++ models/sequelize/client.js
@@ -16,5 +16,20 @@ module.exports = (sequelize, DataTypes) => {
         charset: 'latin1',
         collate: 'latin1_bin',
     });
+
+    Clients.associate = (db) => {
+        // Clients -> Quotes
+        db.Clients.hasMany(db.Quotes, { foreignKey: 'clientId', allowNull: false });
+        db.Quotes.belongsTo(db.Clients, { foreignKey: 'clientId', allowNull: false });
+
+        // Clients -> Contacts
+        db.Clients.hasMany(db.Contacts, { foreignKey: 'clientId', allowNull: false });
+        db.Contacts.belongsTo(db.Clients, { foreignKey: 'clientId', allowNull: false });
+
+        // Clients -> Jobs
+        db.Clients.hasMany(db.Jobs, { foreignKey: 'clientId', allowNull: false });
+        db.Jobs.belongsTo(db.Clients, { foreignKey: 'clientId', allowNull: false });
+    };
+
 return Clients;
 }
