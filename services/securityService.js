f2d647e76a25e5a0ee9b0c6281b43f009c9ecd4a
CappyTech <133281784+CappyTech@users.noreply.github.com>
2024-12-18 19:06:35 +0000

    Update securityService.js

--- services/securityService.js
+++ services/securityService.js
@@ -6,8 +6,8 @@ const securityService = [
     helmet.contentSecurityPolicy({
         directives: {
             defaultSrc: ["'self'", "https://app.heroncs.co.uk"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com", "https://unpkg.com", "https://cdn.datatables.net"],
-            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://unpkg.com", "https://cdn.datatables.net", "https://code.jquery.com"],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com", "https://unpkg.com"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://unpkg.com"],
             fontSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.gstatic.com", "https://fonts.googleapis.com"],
             imgSrc: [
                 "'self'",
@@ -19,8 +19,7 @@ const securityService = [
                 "https://a.tile.openstreetmap.org",
                 "https://b.tile.openstreetmap.org",
                 "https://c.tile.openstreetmap.org",
-                "https://unpkg.com",
-                "https://cdn.datatables.net"
+                "https://unpkg.com"
             ],
             connectSrc: [
                 "'self'",
