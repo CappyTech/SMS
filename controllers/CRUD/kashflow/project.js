b5c935e0e4cf38049916cfa3bb42ff2fbfe579f8
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-01-21 17:52:04 +0000

    Update project.js

--- controllers/CRUD/kashflow/project.js
+++ controllers/CRUD/kashflow/project.js
@@ -2,20 +2,33 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../../../services/loggerService');
 const path = require('path');
+const fs = require('fs');
 const db = require('../../../services/kashflowDatabaseService');
 const authService = require('../../../services/authService');
+
+const projectsDir = path.join(__dirname, '../../../Projects');
+if (!fs.existsSync(projectsDir)) {
+    fs.mkdirSync(projectsDir, { recursive: true });
+    fs.chmodSync(projectsDir, 0o700); // Only owner can read/write/execute
+}
+
 const readProject = async (req, res, next) => {
     try {
         const Project = await db.KF_Projects.findByPk(req.params.uuid);
 
         if (!Project) {
             req.flash('error', 'Project not found.');
-            return res.redirect('/dashboard/KFproject');
+            return res.redirect('/dashboard/project');
         }
+
+        const projectDir = path.join(projectsDir, Project.number.toString());
+        const files = fs.readdirSync(projectDir);
 
         res.render(path.join('kashflow', 'viewProject'), {
             title: 'Project Overview',
             Project,
+            files,
+            number: Project.number
         });
     } catch (error) {
         logger.error('Error reading kashflow Project:' + error.message);
@@ -24,6 +37,6 @@ const readProject = async (req, res, next) => {
     }
 }
 
-router.get('/kf/project/read/:uuid', authService.ensureAuthenticated, authService.ensureRole('admin'), readProject);
+router.get('/project/read/:uuid', authService.ensureAuthenticated, authService.ensureRole('admin'), readProject);
 
 module.exports = router;
