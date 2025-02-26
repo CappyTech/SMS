5f65f07362c3b7f98d276dfa6680bab480c58f34
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-02-26 13:30:21 +0000

    Create swagger.js

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'SMS API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./app.js'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
