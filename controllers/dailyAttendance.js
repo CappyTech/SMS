b0991f697b83d1a0a3899b70799cda99667f4da5
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-02-26 13:50:37 +0000

    Update dailyAttendance.js

--- controllers/dailyAttendance.js
+++ controllers/dailyAttendance.js
@@ -1,3 +1,9 @@
+/**
+ * @fileoverview Controller for handling daily attendance routes.
+ * 
+ * @module controllers/dailyAttendance
+ */
+
 const express = require('express');
 const router = express.Router();
 const moment = require('moment');
@@ -7,6 +13,16 @@ const attendanceService = require('../services/attendanceService');
 const authService = require('../services/authService');
 const db = require('../services/sequelizeDatabaseService');
 
+/**
+ * Get daily attendance for a specific date or the current date if not provided.
+ * 
+ * @async
+ * @function getDailyAttendance
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {Promise<void>}
+ */
 const getDailyAttendance = async (req, res, next) => {
     const date = req.params.date || moment().format('YYYY-MM-DD'); // Default to today
     try {
@@ -27,6 +43,18 @@ const getDailyAttendance = async (req, res, next) => {
     }
 };
 
+/**
+ * Route to get daily attendance.
+ * 
+ * @name get/daily/:date?
+ * @function
+ * @memberof module:controllers/dailyAttendance
+ * @inner
+ * @param {string} [date] - Optional date parameter in 'YYYY-MM-DD' format.
+ * @param {Function} authService.ensureAuthenticated - Middleware to ensure user is authenticated.
+ * @param {Function} authService.ensureRole - Middleware to ensure user has 'admin' role.
+ * @param {Function} getDailyAttendance - Controller function to handle the route.
+ */
 router.get('/daily/:date?', authService.ensureAuthenticated, authService.ensureRole('admin'), getDailyAttendance);
 
 module.exports = router;
