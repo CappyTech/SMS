1ce1764c8a2baa5f766ecddf62e377381c09a510
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-03-03 15:54:30 +0000

    Update loggerService.js

--- services/loggerService.js
+++ services/loggerService.js
@@ -1,50 +1,4 @@
 const { createLogger, format, transports } = require('winston');
-<<<<<<< HEAD
-const { combine, timestamp, json } = format;
-const DailyRotateFile = require('winston-daily-rotate-file');
-const fs = require('fs');
-const path = require('path');
-
-const logDir = path.join(__dirname, '../logs');
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
-}
-
-const fileOptions = {
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '30d'
-};
-
-const applicationTransport = new DailyRotateFile({
-    filename: path.join(logDir, 'application-%DATE%.log'),
-    level: 'debug',
-    ...fileOptions
-});
-
-const infoTransport = new DailyRotateFile({
-    filename: path.join(logDir, 'info-%DATE%.log'),
-    level: 'info',
-    ...fileOptions
-});
-
-const warnTransport = new DailyRotateFile({
-    filename: path.join(logDir, 'warn-%DATE%.log'),
-    level: 'warn',
-    ...fileOptions
-});
-
-const errorTransport = new DailyRotateFile({
-    filename: path.join(logDir, 'errors-%DATE%.log'),
-    level: 'error',
-    maxFiles: '60d',
-    ...fileOptions
-});
-
-const baseLogger = createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-=======
 const { combine, timestamp, printf, colorize } = format;
 
 /**
@@ -69,37 +23,15 @@ const logFormat = printf(({ level, message, timestamp }) => {
  */
 const logger = createLogger({
     level: 'debug',
->>>>>>> parent of acd4089 (Update loggerService.js)
     format: combine(
         colorize(),
         timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-<<<<<<< HEAD
-        format.printf(({ level, message, timestamp, ...meta }) => {
-            let metaString = '';
-            if (Object.keys(meta).length) {
-                try {
-                    metaString = JSON.stringify(meta, null, 2);
-                } catch (error) {
-                    metaString = '[Error serializing metadata]';
-                }
-            }
-            return `${timestamp} [${level.toUpperCase()}] ${message} ${metaString}`;
-        })
-    ),
-    transports: [
-        applicationTransport,
-        infoTransport,
-        warnTransport,
-        errorTransport
-    ]
-=======
         logFormat
     ),
     transports: [
         new transports.Console(), // Log to the console
         new transports.File({ filename: 'app.log' }) // Log to a file
     ],
->>>>>>> parent of acd4089 (Update loggerService.js)
 });
 
 if (process.env.NODE_ENV !== 'production') {
