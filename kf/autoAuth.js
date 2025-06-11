543ae968c47dac95d647bb41a5493dbfe97f9831
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-06-11 14:05:26 +0100

    Update autoAuth.js

--- kf/autoAuth.js
+++ kf/autoAuth.js
@@ -2,35 +2,37 @@ const soap = require('soap');
 require('dotenv').config({ path: '../.env' });
 const logger = require('../services/loggerService');
 
-function authenticate(context = 'main thread', callback) {
-  soap.createClient('https://securedwebapp.com/api/service.asmx?WSDL', (error, client) => {
-    if (error) {
-      logger.error(`[${context}] Error creating SOAP client: ${error}`);
-      return callback(error);
-    }
-
-    logger.info(`[${context}] SOAP client created successfully.`);
-
-    const authParams = {
-      UserName: process.env.KFUSERNAME,
-      Password: process.env.KFPASSWORD,
-      appName: process.env.appName,
-      AutoAuthKey: process.env.autoAuthKey
-    };
-
-    client.AutoAuthIP(authParams, (error, authResult) => {
-      if (error) {
-        logger.error(`[${context}] Error calling AutoAuthIP method: ${error}`);
-        return callback(error);
+function authenticate(context = 'main thread') {
+  return new Promise((resolve, reject) => {
+    soap.createClient('https://securedwebapp.com/api/service.asmx?WSDL', (err, client) => {
+      if (err) {
+        logger.error(`[${context}] Error creating SOAP client: ${err}`);
+        return reject(err);
       }
 
-      if (authResult.Status === 'OK') {
-        logger.info(`[${context}] AutoAuthIP successful.`);
-        return callback(null, client);
-      } else {
-        logger.error(`[${context}] Failed to authenticate: ${authResult.StatusDetail}`);
-        return callback(new Error(authResult.StatusDetail));
-      }
+      logger.info(`[${context}] SOAP client created successfully.`);
+
+      const authParams = {
+        UserName: process.env.KFUSERNAME,
+        Password: process.env.KFPASSWORD,
+        appName: process.env.appName,
+        AutoAuthKey: process.env.autoAuthKey
+      };
+
+      client.AutoAuthIP(authParams, (err, result) => {
+        if (err) {
+          logger.error(`[${context}] Error calling AutoAuthIP: ${err}`);
+          return reject(err);
+        }
+
+        if (result.Status === 'OK') {
+          logger.info(`[${context}] AutoAuthIP successful.`);
+          return resolve(client);
+        } else {
+          logger.error(`[${context}] AutoAuthIP failed: ${result.StatusDetail}`);
+          return reject(new Error(result.StatusDetail));
+        }
+      });
     });
   });
 }
